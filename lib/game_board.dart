8877189b8ebba48f4268a2b1c779109523b17506
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-11-25 21:54:05 +0545

    listing chess piece

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,3 +1,4 @@
+import 'package:chess_game/components/piece.dart';
 import 'package:chess_game/components/square.dart';
 import 'package:chess_game/helper/helper_methods.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,23 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
+  // a 2-dimensional list representing the chess board,
+  //with each position possibily containing a chess piece
+
+  late List<List<ChessPiece?>> board;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  //initialize the board
+  void _initalizeBoard() {}
+  //create a piece
+  ChessPiece myPawn = ChessPiece(
+      type: ChessPieceType.pwan,
+      isWhite: true,
+      imagePath: 'lib/images/pawn.png');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +37,10 @@ class _GameBoardState extends State<GameBoard> {
           gridDelegate:
               SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
           itemBuilder: (context, index) {
-            return Square(isWhite: isWhite(index));
+            return Square(
+              isWhite: isWhite(index),
+              piece: myPawn,
+            );
           }),
     );
   }
